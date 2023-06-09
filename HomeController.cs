2d3cfe4e6c098db13fdf70c0964ae4ee7fd073c7
maliqharris <127056176+maliqharris@users.noreply.github.com>
2023-06-09 14:56:44 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Survey1.Models;
+
+namespace Survey1.Controllers;
+
+public class HomeController : Controller
+{
+    static Survey NewUser;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("process")]
+    public IActionResult Privacy(Survey user)
+    {
+        NewUser = user;
+        return RedirectToAction("Result");
+    }
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+        return View(NewUser);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
